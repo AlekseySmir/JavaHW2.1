ab9f80b35dd235068edd3b9ba271e8db00a3691e
Алексей Смирнов <AlekseySmir40@bk.ru>
2022-07-14 13:17:38 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 400;
+        int bonus = 20;
+        int miles = price / bonus;
+        System.out.println("Бонусные мили:" + miles);
+    }
+}
